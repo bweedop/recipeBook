65faad82933d78743d970a2d97740f39ad0911f9
bweedop <kbweedop@gmail.com>
2020-11-29 14:46:45 -0700

    Add new files from build

---
+++ build/precache-manifest.45cd497f8bbe6898065e6f9137a4f671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c7cac1c7555b85e163fb194c1f45d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c621276a2f8a282cfed9",
+    "url": "/static/css/main.6bda0754.chunk.css"
+  },
+  {
+    "revision": "a420acdefa4ffc44db7d",
+    "url": "/static/js/2.522a6535.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.522a6535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c621276a2f8a282cfed9",
+    "url": "/static/js/main.6c198a7c.chunk.js"
+  },
+  {
+    "revision": "8367f560e1f0f1b161da",
+    "url": "/static/js/runtime-main.1c488459.js"
+  }
+]);
