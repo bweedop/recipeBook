5f3f16cd0303a951d0254a8bc3289e523377631a
bweedop <kbweedop@gmail.com>
2020-11-30 12:05:15 -0700

    Add recipe functionality added. Need React Router.

---
+++ build/precache-manifest.0d422d653a4df48b950f6d2cd27efbad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73c2176a5a25e69a66e09e135ca78ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec6b68187d4fcce779c0",
+    "url": "/static/css/main.b4fc73f9.chunk.css"
+  },
+  {
+    "revision": "fd0ff596d7b6529b09fc",
+    "url": "/static/js/2.1832a96e.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.1832a96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6b68187d4fcce779c0",
+    "url": "/static/js/main.c42328f9.chunk.js"
+  },
+  {
+    "revision": "8367f560e1f0f1b161da",
+    "url": "/static/js/runtime-main.1c488459.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45cd497f8bbe6898065e6f9137a4f671.js"
+  "/precache-manifest.0d422d653a4df48b950f6d2cd27efbad.js"
 );
 
 self.addEventListener('message', (event) => {
